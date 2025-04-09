798cfdd07b03fa10ba5790ddac876d052ad99778
Vic Cherubini <vcherubini@gmail.com>
2025-04-09 15:37:23 -0500

    first test

--- tests/Request/UploadFileRequestTest.php
+++ tests/Request/UploadFileRequestTest.php
@@ -2,6 +2,24 @@
 
 namespace OneToMany\StorageBundle\Tests\Request;
 
-final class UploadFileRequestTest
+use OneToMany\StorageBundle\Request\UploadFileRequest;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+
+#[Group('UnitTests')]
+final class UploadFileRequestTest extends TestCase
 {
+
+    public function testGettingUrlUsesCanonicalUrlWhenCustomUrlIsEmpty(): void
+    {
+        $canonicalUrl = 'https://canonical-url.com/file.jpeg';
+
+        $request = new UploadFileRequest(...[
+            'filePath' => 'file.jpeg',
+            'remoteKey' => 'file.jpeg',
+            'customUrl' => null,
+        ]);
+
+        $this->assertEquals($canonicalUrl, $request->getUrl($canonicalUrl));
+    }
 }
