e3489763cf327f421f3f3c7b6f23cbb41452cde2
Vic Cherubini <vcherubini@gmail.com>
2025-04-09 15:46:24 -0500

    tests

--- tests/Request/UploadFileRequestTest.php
+++ tests/Request/UploadFileRequestTest.php
@@ -3,6 +3,7 @@
 namespace OneToMany\StorageBundle\Tests\Request;
 
 use OneToMany\StorageBundle\Request\UploadFileRequest;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 
@@ -12,14 +13,54 @@ final class UploadFileRequestTest extends TestCase
 
     public function testGettingUrlUsesCanonicalUrlWhenCustomUrlIsEmpty(): void
     {
-        $canonicalUrl = 'https://canonical-url.com/file.jpeg';
+        $remoteKey = 'file.jpeg';
+        $canonicalUrl = 'https://canonical.example/'.$remoteKey;
 
         $request = new UploadFileRequest(...[
-            'filePath' => 'file.jpeg',
-            'remoteKey' => 'file.jpeg',
-            'customUrl' => null,
+            'filePath' => $remoteKey,
+            'remoteKey' => $remoteKey,
         ]);
 
         $this->assertEquals($canonicalUrl, $request->getUrl($canonicalUrl));
+    }
+
+    #[DataProvider('providerCustomUrlAndStrippedCustomUrl')]
+    public function testGettingUrlStripsEndingSlashFromCustomUrl(
+        string $customUrl,
+        string $strippedCustomUrl,
+    ): void
+    {
+        $remoteKey = 'file.jpeg';
+
+        $objectUrl = $strippedCustomUrl.'/'.$remoteKey;
+        $canonicalUrl = 'https://canonical.example/'.$remoteKey;
+
+        $request = new UploadFileRequest(...[
+            'filePath' => $remoteKey,
+            'remoteKey' => $remoteKey,
+            'customUrl' => $customUrl,
+        ]);
+
+        $this->assertNotEquals($objectUrl, $canonicalUrl);
+        $this->assertEquals($objectUrl, $request->getUrl($canonicalUrl));
+    }
+
+    /**
+     * @return list<list<non-empty-string>>
+     */
+    public static function providerCustomUrlAndStrippedCustomUrl(): array
+    {
+        $provider = [
+            [
+                'https://custom.example',
+                'https://custom.example',
+            ],
+            [
+                'https://custom.example/',
+                'https://custom.example',
+            ],
+        ];
+
+        return $provider;
     }
 }
