f8893dee229c6a10df501bf35c679da805168715
Vic Cherubini <vcherubini@gmail.com>
2025-04-09 14:10:53 -0500

    improved formatting configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,10 +1,25 @@
 <?php
 
-$finder = new \PhpCsFixer\Finder()->exclude('config')->in(__DIR__);
+$finder = new PhpCsFixer\Finder()
+    ->exclude([
+        'config',
+        'var',
+    ])
+    ->in([
+        './src',
+        './tests',
+    ]);
 
-return new \PhpCsFixer\Config()->setFinder($finder)->setRules([
-    '@Symfony' => true,
-    'phpdoc_align' => [
-        'align' => 'left',
-    ],
-]);
+return new PhpCsFixer\Config()
+    ->setFinder($finder)
+    ->setRules([
+        '@Symfony' => true,
+        'global_namespace_import' => [
+            'import_classes' => false,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+    ]);
