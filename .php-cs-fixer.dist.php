2ff7b4e3a7c36ad6bc0f6814b6c46c30071f8608
Vic Cherubini <vcherubini@gmail.com>
2025-11-22 18:35:34 -0600

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,30 +1,42 @@
 <?php
 
-$finder = new PhpCsFixer\Finder()
-    ->exclude([
-        './config/',
-        './var/',
-    ])
-    ->in([
-        './src/',
-        './tests/',
-    ]);
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfig;
 
-return new PhpCsFixer\Config()
-    ->setFinder($finder)
-    ->setRules([
-        '@Symfony' => true,
-        'global_namespace_import' => [
-            'import_classes' => false,
-            'import_constants' => true,
-            'import_functions' => true,
+$finder = new Finder();
+
+$finder->in([
+    './',
+    './src/',
+    './tests/',
+]);
+
+$finder->exclude([
+    'config',
+    'vendor',
+]);
+
+$config = new Config()->setParallelConfig(...[
+    'config' => new ParallelConfig(4),
+]);
+
+$config->setFinder($finder);
+$config->setRules([
+    '@Symfony' => true,
+    'global_namespace_import' => [
+        'import_classes' => false,
+        'import_constants' => true,
+        'import_functions' => true,
+    ],
+    'phpdoc_align' => [
+        'align' => 'left',
+    ],
+    'phpdoc_to_comment' => [
+        'ignored_tags' => [
+            'disregard',
         ],
-        'phpdoc_align' => [
-            'align' => 'left',
-        ],
-        'phpdoc_to_comment' => [
-            'ignored_tags' => [
-                'disregard',
-            ],
-        ],
-    ]);
+    ],
+]);
+
+return $config;
