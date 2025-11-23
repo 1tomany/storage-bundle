0b4dc41e969474ce5e2de3d8e477c03cf2de43ce
Vic Cherubini <vcherubini@gmail.com>
2025-11-22 20:13:55 -0600

    wip

--- src/Contract/Client/StorageClientInterface.php
+++ src/Contract/Client/StorageClientInterface.php
@@ -3,11 +3,13 @@
 namespace OneToMany\StorageBundle\Contract\Client;
 
 use OneToMany\StorageBundle\Contract\Request\DownloadFileRequestInterface;
+use OneToMany\StorageBundle\Contract\Request\UploadFileRequestInterface;
 use OneToMany\StorageBundle\Contract\Response\DownloadedFileResponseInterface;
+use OneToMany\StorageBundle\Contract\Response\UploadedFileResponseInterface;
 
 interface StorageClientInterface
 {
     public function download(DownloadFileRequestInterface $request): DownloadedFileResponseInterface;
 
-    // public function upload(UploadFileRequest $request): RemoteFileRecord;
+    public function upload(UploadFileRequestInterface $request): UploadedFileResponseInterface;
 }
