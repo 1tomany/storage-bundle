720c416eb9b95d163119038e1711593d41a7dfd3
Vic Cherubini <vcherubini@gmail.com>
2025-11-22 20:24:25 -0600

    wip

--- src/Action/UploadFileAction.php
+++ src/Action/UploadFileAction.php
@@ -2,18 +2,22 @@
 
 namespace OneToMany\StorageBundle\Action;
 
+use OneToMany\StorageBundle\Contract\Action\UploadFileActionInterface;
+use OneToMany\StorageBundle\Contract\Client\StorageClientInterface;
+use OneToMany\StorageBundle\Contract\Request\UploadFileRequestInterface;
+use OneToMany\StorageBundle\Contract\Response\UploadedFileResponseInterface;
 use OneToMany\StorageBundle\Record\RemoteFileRecord;
 use OneToMany\StorageBundle\Request\UploadFileRequest;
 use OneToMany\StorageBundle\Service\StorageServiceInterface;
 
-readonly class UploadFileAction
+readonly class UploadFileAction implements UploadFileActionInterface
 {
-    public function __construct(private StorageServiceInterface $storageService)
+    public function __construct(private StorageClientInterface $storageClient)
     {
     }
 
-    public function act(UploadFileRequest $request): RemoteFileRecord
+    public function act(UploadFileRequestInterface $request): UploadedFileResponseInterface
     {
-        return $this->storageService->upload($request);
+        return $this->storageClient->upload($request);
     }
 }
