0f25603b49745af48506a7eea4327d35f2a04793
Vic Cherubini <vcherubini@gmail.com>
2025-11-22 20:51:20 -0600

    wip

--- tests/Request/UploadFileRequestTest.php
+++ tests/Request/UploadFileRequestTest.php
@@ -5,6 +5,7 @@ namespace OneToMany\StorageBundle\Tests\Request;
 use OneToMany\StorageBundle\Request\UploadFileRequest;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Path;
 
 use function mime_content_type;
 
@@ -16,16 +17,16 @@ final class UploadFileRequestTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->path = __DIR__.'/../data/php-logo.png';
+        $this->path = Path::canonicalize(__DIR__.'/../data/php-logo.png');
     }
 
     public function testCreatingPublicRequestSetsAclAsPublic(): void
     {
-        $this->assertTrue(UploadFileRequest::public($this->path, mime_content_type($this->path), $this->path)->isPublic());
+        $this->assertTrue(UploadFileRequest::public($this->path, (string) mime_content_type($this->path), $this->path)->isPublic());
     }
 
     public function testCreatingPrivateRequestSetsAclAsPublic(): void
     {
-        $this->assertFalse(UploadFileRequest::private($this->path, mime_content_type($this->path), $this->path)->isPublic());
+        $this->assertFalse(UploadFileRequest::private($this->path, (string) mime_content_type($this->path), $this->path)->isPublic());
     }
 }
