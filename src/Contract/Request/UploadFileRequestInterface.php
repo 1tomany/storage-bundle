f6f5167bc1540066223fd95fcd745d8d277f0bed
Vic Cherubini <vcherubini@gmail.com>
2025-11-22 20:04:39 -0600

    wip

---
+++ src/Contract/Request/UploadFileRequestInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OneToMany\StorageBundle\Contract\Request;
+
+interface UploadFileRequestInterface
+{
+    /**
+     * @return non-empty-string
+     */
+    public function getPath(): string;
+
+    public function setPath(string $path): static;
+
+    /**
+     * @return non-empty-string
+     */
+    public function getMimeType(): string;
+
+    public function setMimeType(string $mimeType): static;
+
+    /**
+     * @return non-empty-string
+     */
+    public function getKey(): string;
+
+    public function setKey(string $key): static;
+
+    public function markAsPublic(): static;
+
+    public function markAsPrivate(): static;
+}
