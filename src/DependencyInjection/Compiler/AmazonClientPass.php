dfc231a978cc5c9b3b49103a304b89097c64e7bc
Vic Cherubini <vcherubini@gmail.com>
2026-03-05 14:37:17 -0600

    wip

--- src/DependencyInjection/Compiler/AmazonClientPass.php
+++ src/DependencyInjection/Compiler/AmazonClientPass.php
@@ -5,6 +5,7 @@ namespace OneToMany\StorageBundle\DependencyInjection\Compiler;
 use Aws\S3\S3ClientInterface;
 use OneToMany\StorageBundle\Client\Amazon\AmazonClient;
 use OneToMany\StorageBundle\Exception\InvalidArgumentException;
+use OneToMany\StorageBundle\Exception\RuntimeException;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -22,27 +23,32 @@ class AmazonClientPass implements CompilerPassInterface
             return;
         }
 
-        /** @var non-empty-list<array<non-empty-string, mixed>> $storageConfig */
-        $storageConfig = $container->getExtensionConfig('onetomany_storage');
-
-        print_r($storageConfig);
+        if (!interface_exists(S3ClientInterface::class, true)) {
+            throw new RuntimeException(sprintf('The AWS SDK is required to use "%s" but was not found. Try running "composer require aws/aws-sdk-php-symfony" to install it.', AmazonClient::class));
+        }
 
         $s3ClientArg = $container
             ->getDefinition(AmazonClient::class)
             ->getArgument('$s3Client');
 
         if (!$s3ClientArg instanceof Reference) {
-            throw new InvalidArgumentException(sprintf('The "$s3Client" argument required by "%s" is expected to be a reference to a service.', AmazonClient::class));
+            throw new InvalidArgumentException(sprintf('The "$s3Client" argument required by "%s::__construct()" is expected to be a reference to a service.', AmazonClient::class));
         }
 
         $s3ClientId = $s3ClientArg->__toString();
 
-        if (!$container->has($s3ClientId)) {
+        if (!$container->hasDefinition($s3ClientId)) {
             throw new InvalidArgumentException(sprintf('The service "%s" required by "%s" does not exist.', $s3ClientId, AmazonClient::class));
         }
 
-        if (!$container->get($s3ClientId) instanceof S3ClientInterface) {
-            throw new InvalidArgumentException(sprintf('The service "%s" required by "%s" is not an instance of "%s". Try running "composer require aws/aws-sdk-php-symfony" to install the AWS SDK.', $s3ClientId, AmazonClient::class, S3ClientInterface::class));
+        $s3ClientArgClass = $container->getDefinition($s3ClientId)->getClass();
+
+        if (null === $s3ClientArgClass) {
+            throw new InvalidArgumentException(sprintf('The "$s3Client" argument required by "%s::__construct()" must reference a class.', AmazonClient::class));
+        }
+
+        if (!\is_a($s3ClientArgClass, S3ClientInterface::class, true)) {
+            throw new InvalidArgumentException(sprintf('The "$s3Client" argument required by "%s::__construct()" must be an instance of "%s", "%s" provided.', AmazonClient::class, S3ClientInterface::class, $s3ClientArgClass));
         }
     }
 }
