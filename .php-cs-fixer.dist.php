678ae77b5e5fa5169def9a3a2b22f79e7db9bcc2
Vic Cherubini <vcherubini@gmail.com>
2026-04-03 10:31:46 -0500

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,34 +7,28 @@ use PhpCsFixer\Runner\Parallel\ParallelConfig;
 $finder = new Finder();
 
 $finder->in([
-    './',
     './src/',
     './tests/',
 ]);
 
-$finder->exclude([
-    'vendor',
-]);
-
-$config = new Config()->setParallelConfig(...[
-    'config' => new ParallelConfig(4),
-]);
-
-$config->setFinder($finder);
-$config->setRules([
-    '@Symfony' => true,
-    'global_namespace_import' => [
-        'import_classes' => false,
-        'import_constants' => true,
-        'import_functions' => true,
-    ],
-    'operator_linebreak' => [
-        'only_booleans' => true,
-        'position' => 'end',
-    ],
-    'phpdoc_align' => [
-        'align' => 'left',
-    ],
-]);
+$config = new Config()
+    ->setFinder($finder)
+    ->setParallelConfig(new ParallelConfig(4))
+    ->setCacheFile('./.build/php-cs-fixer.cache')
+    ->setRules([
+        '@Symfony' => true,
+        'global_namespace_import' => [
+            'import_classes' => false,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'operator_linebreak' => [
+            'only_booleans' => true,
+            'position' => 'end',
+        ],
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+    ]);
 
 return $config;
